1b9ac28ad5c1f0673f82b928afa3a0c17bc9b5a1
gangslee <grandnet1225@gmail.com>
2020-08-30 00:29:23 +0900

    Set background gradient with expo

--- Weather.js
+++ Weather.js
@@ -1,21 +1,70 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, StatusBar} from 'react-native';
 import PropTypes from 'prop-types';
+import {LinearGradient} from 'expo-linear-gradient';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 
+const weatherOptions = {
+  Haze: {
+    iconName: 'weather-hail',
+    gradient: ['#4DA0B0', '#D39D38'],
+  },
+  Thunderstorm: {
+    iconName: '',
+    gradient: [],
+  },
+  Drizzle: {
+    iconName: '',
+    gradient: [],
+  },
+  Rain: {
+    iconName: '',
+    gradient: [],
+  },
+  Snow: {
+    iconName: '',
+    gradient: [],
+  },
+  Atmosphere: {
+    iconName: '',
+    gradient: [],
+  },
+  Clear: {
+    iconName: '',
+    gradient: [],
+  },
+  Clouds: {
+    iconName: '',
+    gradient: [],
+  },
+  Haze: {
+    iconName: '',
+    gradient: [],
+  },
+  Mist: {
+    iconName: '',
+    gradient: [],
+  },
+  Dust: {
+    iconName: '',
+    gradient: [],
+  },
+};
 export default function Weather({temp, condition}) {
+  //   console.log('111111111111');
   return (
-    <View style={styles.container}>
+    <LinearGradient colors={['#4c669f', '#3b5998', '#192f6a']} style={styles.container}>
+      <StatusBar barStyle="light-content" />
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+        <MaterialCommunityIcons size={96} name={weatherOptions['Haze'].iconName} color="white" />
         <Text style={styles.temp}>{temp}</Text>
       </View>
       <View style={styles.halfContainer}></View>
-    </View>
+    </LinearGradient>
   );
 }
 
-Weather.PropTypes = {
+Weather.propTypes = {
   temp: PropTypes.number.isRequired,
   condition: PropTypes.oneOf([
     'Thunderstorm',
@@ -39,6 +88,7 @@ const styles = StyleSheet.create({
   },
   temp: {
     fontSize: 42,
+    color: 'white',
   },
   halfContainer: {
     flex: 1,
